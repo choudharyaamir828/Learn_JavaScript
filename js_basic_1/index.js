a0c5ba03a1beb38c860d76eddf3461e6868a3f63
mohd Amir <choudharyaamir828@gmail.com>
2024-09-08 16:58:43 +0530

    first file added

---
+++ js_basic_1/index.js
@@ -0,0 +1,76 @@
+console.log("javaScript version-3")
+
+    //  loose euality 
+    let a = 12;
+    var b = 12;
+    console.log(a == b);
+    console.log(a === b);
+
+    // // strict Euality
+     let num1 = 30;
+     let num2 = "aamir";
+     console.log(num1 == num2);
+    console.log(num1 === num2);
+     
+
+    // operator
+    // console.log(2+3);
+    // console.log(2-3);
+    // console.log(2*3);
+    // console.log(2/3);
+    // console.log(2**3);
+    // console.log(3%2);
+    // a = 3;
+    // console.log(++a); // 4
+    //  console.log(a++);   //5
+    // a++;                        //6
+    // console.log(a);  //6
+    // --a;            
+    // console.log(a--);  //5
+    // console.log(a);   //4
+
+
+
+
+
+// Array
+// let Name = ['love','aamir','choudhary'];
+// //  console.log(Name[-2]); // not working (undefined )
+//     console.log(Name[2]);
+//     Name[3] = 'hello';
+//     console.log(Name[3]);
+
+
+// object
+//  let person = {
+//     Name : "aamir",
+//     age  : 22
+
+//  };
+
+//  console.log(person.Name);
+
+
+
+//  let a = 5;
+//  console.log(a);
+//  a = "aamir";
+//  console.log(a);
+
+
+// const a = 2;
+ 
+//  a = 13;    // no allowed
+//  console.log(a);
+
+
+
+// var b= 10;
+// console.log(b);   
+// var b= 20;      // we can declare again variable  using var 
+// console.log(b);
+
+// let a = 'love';
+// let b = 20;
+// console.log(a+b);
+
