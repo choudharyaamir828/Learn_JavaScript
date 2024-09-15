6a7ef5185787db7521101c40e79817b6ca398167
mohd Amir <choudharyaamir828@gmail.com>
2024-09-15 22:08:46 +0530

    sum code added

--- js_basic_2/index2.js
+++ js_basic_2/index2.js
@@ -64,72 +64,37 @@
                 // CREATE OBJECT USING CONSTRUCTOR FUNCTION
                 // follow Pascal Notatioln to -> first letter of every word in capital -> NumberOfState
 
-function Rectangle(len,bre){
-    this.length = len;
-    this.breadth = bre;
-    this.drow = function(){
-        console.log("drowing");
-    }
+// function Rectangle(len,bre){
+//     this.length = len;
+//     this.breadth = bre;
+//     this.drow = function(){
+//         console.log("drowing");
+//     }
 
-}
+// }
 
-let rectangleObj1 = new Rectangle(6,7);
-let rectangleObj2 = new Rectangle(8,9);
+// let rectangleObj1 = new Rectangle(6,7);
+// let rectangleObj2 = new Rectangle(8,9);
 
 
 // console.log(rectangleObj1)
 // console.log(rectangleObj2)
 
-rectangleObj1.color = "yellow";  // add property
-console.log(rectangleObj1);
+// rectangleObj1.color = "yellow";  // add property
+// console.log(rectangleObj1);
 
-delete rectangleObj1.color;   // delete property
-console.log(rectangleObj1);  
+// delete rectangleObj1.color;   // delete property
+// console.log(rectangleObj1);  
 
-console.log(rectangleObj2);  
+// console.log(rectangleObj2);  
 
+calSum = function(){
+    let arr = [3,6,2,1,7,10];
+    let sum = 0;
+    for(let i =0;i<6;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("the sum is "+calSum());
